cb32be01a4c57797a8cc9fd528e490c3eea44989
codeDi <baduy9780@gmail.com>
2022-02-14 23:24:18 +0900

    Update meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = useState("")
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
 
   function handleClick() {
-    const memes = memesData.data.memes;
+    const memes = allMemeImages.data.memes
     const randomMeme = memes[Math.floor(Math.random() * memes.length)]
     const newImage = randomMeme.url
-    console.log(newImage)
-    setMemeImage(newImage)
+    setMeme(prevMeme => ({...prevMeme, randomImage: newImage}))
   }
 
   return (
@@ -20,15 +24,17 @@ export default function Meme() {
           className="form--input" 
           name="top-text" 
           placeholder="Top text"
+          value={meme.topText}
         />
         <input
           type="text" 
           className="form--input" 
           name="bottom-text" 
           placeholder="Bottom text"
+          value={meme.bottomText}
         />
-        <button type="button" className="form--button" onClick={handleClick}>Get a new meme image🖼</button>
-        <img className="meme--image" src={memeImage} />
+        <button type="button" className="form--button" onClick={handleClick}>Get a new meme image 🖼</button>
+        <img className="meme--image" src={meme.randomImage} alt="" />
       </form>
     </main>
   )
